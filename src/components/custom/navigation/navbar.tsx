694bbe290d14120e432983f717df501e58f30f37
chordzz <olaifeolawore@gmail.com>
2025-05-21 15:38:52 +0100

    finished pages and responsiveness

--- src/components/custom/navigation/navbar.tsx
+++ src/components/custom/navigation/navbar.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 
 import logo from '@/svgs/cuesoft-logo.svg';
 import btnIcon from '@/svgs/cta-arrow.svg';
+
+import { Menu } from 'lucide-react';
 
 const navLinks = [
   {
@@ -25,11 +30,34 @@ const navLinks = [
 ];
 
 function NavBar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // Close menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setOpenMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  // const handleClick = () => {
+  //   alert('clicked');
+  //   setOpenMenu((prev) => !prev);
+  // };
+
   return (
-    <nav className='mx-auto h-[82px] w-[87.2%] flex items-center justify-between'>
+    <nav className='mx-auto h-[82px] w-[90%] lg:w-[87.2%] flex items-center justify-between relative'>
       <Image src={logo} alt='Cuesoft Logo' className='w-[213.7px] h-[42px]' />
 
-      <ul className='text-16c font-semibold leading-[16px] flex items-center gap-8'>
+      <ul className='text-16c font-semibold leading-[16px] hidden lg:flex items-center gap-8'>
         {navLinks.map((link, index) => (
           <li
             key={index}
@@ -40,10 +68,39 @@ function NavBar() {
         ))}
       </ul>
 
-      <Button variant={'default'} className='btn-main'>
+      <Button variant={'default'} className='btn-main hidden lg:flex'>
         Get Started
         <Image src={btnIcon} alt='Arrow Icon' />
       </Button>
+
+      <Menu
+        className='block lg:hidden cursor-pointer'
+        onClick={() => setOpenMenu((prev) => !prev)}
+      />
+
+      {openMenu === true && (
+        <div
+          ref={menuRef}
+          className='flex flex-col items-center gap-8 p-6 bg-[#16151d] rounded-lg shadow-lg absolute w-full text-center z-10 top-20'
+          onClick={() => setOpenMenu(false)}
+        >
+          <ul className='flex flex-col items-center gap-6'>
+            {navLinks.map((link, index) => (
+              <li
+                key={index}
+                className='hover:border-b-[#CB39C1] pb-1.5 pt-1 border-2 border-transparent cursor-pointer'
+              >
+                {link.name}
+              </li>
+            ))}
+          </ul>
+
+          <Button variant={'default'} className='btn-main w-full'>
+            Get Started
+            <Image src={btnIcon} alt='Arrow Icon' />
+          </Button>
+        </div>
+      )}
     </nav>
   );
 }
